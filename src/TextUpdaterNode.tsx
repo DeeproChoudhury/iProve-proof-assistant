a84e07f7647424cb641110af15d08262ddcbd467
Vlad <vladcoroian2001@gmail.com>
2022-10-16 10:36:55 +0100

    Add support for array of statements inside node

--- src/TextUpdaterNode.tsx
+++ src/TextUpdaterNode.tsx
@@ -1,10 +1,13 @@
-import { ReactNode, useCallback } from 'react';
+import { ReactNode, useCallback, useState } from 'react';
 import { Handle, Position } from 'reactflow';
 
 function TextUpdaterNode({ data }: any) {
-  const onChange = useCallback((evt: any) => {
-    console.log(evt.target.value);
-  }, []);
+  const [statements, setStatements] = useState([""]);
+
+  const onChange = useCallback((evt: any, updated: number) => {
+    statements[updated] = evt.target.value;
+    setStatements(statements);
+  }, [statements]);
 
   const componentStyle = data.type + "-node";
   const targetHandle: ReactNode = <Handle type="target" position={Position.Top} />;
@@ -13,9 +16,10 @@ function TextUpdaterNode({ data }: any) {
   return (
     <div className={componentStyle}>
       {componentStyle !== "given-node" && targetHandle}
-      <div>
-        <input onChange={onChange} />
-        <button onClick={() => {data.delete(`${data.id}`)}} style={{marginLeft: '5px'}}>Delete</button>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        {statements.map((s, index) => <input onChange={e => onChange(e, index)} style={{marginTop: '5px'}}/>)}
+        <button onClick={() => {data.delete(`${data.id}`)}} style={{marginTop: '5px'}}>Delete</button>
+        <button onClick={() => {setStatements([...statements, ''])}} style={{marginTop: '5px'}}>Add Statement</button>
       </div>
       {componentStyle !== "goal-node" && sourceHandle}
     </div>
