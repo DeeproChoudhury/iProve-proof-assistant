53257f363efceaaddba29ebbe899942fe7201817
Haotian Wu <hw2220@cloud-vm-40-83.doc.ic.ac.uk>
2022-10-18 13:18:25 +0100

    added loading screen for z3 to load

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css';
 // import Flow from './Flow.js';
-import { Z3Solver } from './Solver';
-import { useEffect } from 'react';
+import Z3Solver from './Solver';
+import { useEffect, useState } from 'react';
 import Flow from './Flow';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    Z3Solver.initZ3();
+    Z3Solver.initZ3().then(() => setLoading(false));
+
     return () => {  };
   }, [])
-
+  
   return (
+    <> 
+    {loading === false ? (
     <div style={{backgroundColor: '#B8CEFF'}}>
       <div className='header'>
         iProve
@@ -19,8 +24,14 @@ const App = () => {
         <Flow />
       </div>
       <Z3Solver.useZ3Button />
-    </div>
-  );
+    </div>) 
+    : (
+    <div className='header'>
+      <h1>LOADING :-)</h1>
+    </div>)
+    
+    }
+    </>);
 }
 
 export default App;
--- src/Solver.tsx
+++ src/Solver.tsx
@@ -1,28 +1,8 @@
 import { Arith, ContextCtor, init, IntCreation } from 'z3-solver';
 
-const solver = async () => {
-    const {
-        //Z3, // Low-level C-like API
-        Context, // High-level Z3Py-like API
-    } = await init();
-    const { Solver, Int, And } = Context('main');
-
-    const x = Int.const('x');
-
-    const solver = new Solver();
-    solver.add(And(x.ge(0), x.le(9)));
-    console.log(await solver.check());
-}
-
-export default solver;
-
 export namespace Z3Solver {
     
     var HighLevelContext : ContextCtor | undefined;
-    // var Z3Int : IntCreation<"m">
-    // var Z3And : any;
-    // var Z3Or : any;
-    // var Z3Implies : any;
 
     export async function initZ3() {
         const {
@@ -107,3 +87,5 @@ export namespace Z3Solver {
         )
     }
 }
+
+export default Z3Solver;
