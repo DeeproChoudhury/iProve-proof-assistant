3d8d5570b806e6c6e8678c26b4fad1a94f39f511
Vlad <vladcoroian2001@gmail.com>
2022-10-19 18:46:27 +0100

    Improve style of buttons on node

--- src/TextUpdaterNode.tsx
+++ src/TextUpdaterNode.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useCallback, useState } from 'react';
 import { Handle, Position } from 'reactflow';
-import { Box, Heading, Button, Text } from '@chakra-ui/react';
-
+import { Box, Heading, Button, Text, IconButton } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 
 function TextUpdaterNode({ data }: any) {
   const onChange = useCallback((evt: any, updated: number) => {
@@ -17,15 +17,33 @@ function TextUpdaterNode({ data }: any) {
   const goalTitle: ReactNode = <Heading textAlign={['center']} as='h6' size='xs'>Goal</Heading>
   const firstProofStep: any = data.statements.findIndex((s: any) => !s.isGiven);
   const lastProofStep: any = data.statements.findLastIndex((s: any) => !s.isGiven);
-  
+
   return (
     <Box className={componentStyle}>
       {componentStyle !== "given-node" && targetHandle}
-      <Text>Givens</Text>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text>Givens</Text>
+        <IconButton
+          variant='outline'
+          aria-label='Add given'
+          size='xs'
+          onClick={() => { data.addGiven(`${data.id}`) }}
+          icon={<AddIcon />}
+        />
+      </div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         {data.statements.map((s: any, index: number) => s.isGiven && <input onChange={e => onChange(e, index)} style={{ marginTop: '5px' }} key={index} value={s.value} />)}
       </div>
-      <Text>Proof Steps</Text>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px' }}>
+        <Text>Proof Steps</Text>
+        <IconButton
+          variant='outline'
+          aria-label='Add proof step'
+          size='xs'
+          onClick={() => { data.addProofStep(`${data.id}`) }}
+          icon={<AddIcon />}
+        />
+      </div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         {componentStyle === "given-node" && givenTitle}
         {componentStyle === "goal-node" && goalTitle}
@@ -40,10 +58,8 @@ function TextUpdaterNode({ data }: any) {
         }
         <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
           <Button size='xs' colorScheme='blackAlpha' onClick={() => { data.delete(`${data.id}`) }}>Delete</Button>
-          {data.statements.length >= 3 && !isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => setCollapsed(true)}>Hide</Button>}
+          {data.statements.filter((s: any) => !s.isGiven).length >= 3 && !isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => setCollapsed(true)}>Hide</Button>}
           {isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => { setCollapsed(false) }}>Show</Button>}
-          <Button size='xs' colorScheme='blackAlpha' onClick={() => { data.addProofStep(`${data.id}`) }}>Add Proof Step</Button>
-          <Button size='xs' colorScheme='blackAlpha' onClick={() => { data.addGiven(`${data.id}`) }}>Add Given</Button>
         </div>
       </div>
       {componentStyle !== "goal-node" && sourceHandle}
