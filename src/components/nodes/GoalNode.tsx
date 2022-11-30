e085ee4b9b84fe5e5268ae8d1d48800411e744be
Vlad Coroian <vladcoroian2001@gmail.com>
2022-11-30 15:58:59 +0000

    Add support for horizontal node resizing

--- src/components/nodes/GoalNode.tsx
+++ src/components/nodes/GoalNode.tsx
@@ -1,42 +1,78 @@
 import { Box, Button } from "@chakra-ui/react";
-import { Handle, NodeProps, Position } from "reactflow";
+import { useState, useEffect } from "react";
+import Moveable from "react-moveable";
+import { NodeProps } from "reactflow";
 import { StatementNodeData } from "../../types/Node";
 import StatementList from "../StatementList";
 import { DeleteNodePopover } from "./GeneralNode";
 import { NodeHandle } from "./NodeHandle";
 
 export default function GoalNode({ data }: NodeProps<StatementNodeData>) {
-  return (
-    <Box className="goal-node">
-      
-      {/* START : Top Handle */}
-      <NodeHandle type="target" />
-      {/* END : Top Handle */}
+  const [target, setTarget] = useState<any>();
+  const [frame] = useState<any>({
+    translate: [0, 0],
+  });
+  useEffect(() => {
+    return setTarget(document.querySelector(".goal-node")!);
+  }, []);
 
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-      {data.correctImplication === undefined &&
-      <Button colorScheme='whatsapp' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-        Check incoming implications
-      </Button>}
-      {data.correctImplication === "valid" &&
-        <Button colorScheme='whatsapp' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-          Check passed. Check again?
-        </Button>}
-      {data.correctImplication === "invalid" &&
-        <Button colorScheme='red' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-          Check failed. Check again?
-        </Button>}
-      </div>
-      <StatementList
-        title="Goals"
-        statements={data.givens}
-        callbacks={data.thisNode.givens}
-        afterStatementEdit={data.thisNode.checkSyntax}
+  return (
+    <div>
+      <Box className="goal-node">
+
+        {/* START : Top Handle */}
+        <NodeHandle type="target" />
+        {/* END : Top Handle */}
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {data.correctImplication === undefined &&
+            <Button colorScheme='whatsapp' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check incoming implications
+            </Button>}
+          {data.correctImplication === "valid" &&
+            <Button colorScheme='whatsapp' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check passed. Check again?
+            </Button>}
+          {data.correctImplication === "invalid" &&
+            <Button colorScheme='red' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check failed. Check again?
+            </Button>}
+        </div>
+        <StatementList
+          title="Goals"
+          statements={data.givens}
+          callbacks={data.thisNode.givens}
+          afterStatementEdit={data.thisNode.checkSyntax}
+        />
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
+          <DeleteNodePopover deleteNode={data.thisNode.delete} />
+          {/* <Button size='xs' colorScheme='blackAlpha' onClick={data.thisNode.checkSyntax}>Check Syntax</Button> */}
+        </div>
+      </Box>
+
+      {/* BEGIN: Moveable component to allow horizontal resizing */}
+      <Moveable
+        target={target}
+        resizable={true}
+        keepRatio={false}
+        throttleResize={1}
+        renderDirections={["e", "w"]}
+        edge={false}
+        zoom={1}
+        origin={false}
+        padding={{ "left": 0, "top": 0, "right": 0, "bottom": 0 }}
+        onResizeStart={e => {
+          e.setOrigin(["%", "%"]);
+          e.dragStart && e.dragStart.set(frame.translate);
+        }}
+        onResize={e => {
+          const beforeTranslate = e.drag.beforeTranslate;
+          frame.translate = beforeTranslate;
+          e.target.style.width = `${e.width}px`;
+          e.target.style.transform = `translate(${beforeTranslate[0]}px, 0px)`;
+        }}
       />
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
-        <DeleteNodePopover deleteNode={data.thisNode.delete} />
-        {/* <Button size='xs' colorScheme='blackAlpha' onClick={data.thisNode.checkSyntax}>Check Syntax</Button> */}
-      </div>
-    </Box>
+      {/* END: Moveable component to allow horizontal resizing */}
+    </div>
   )
 }
--- src/components/nodes/ProofNode.tsx
+++ src/components/nodes/ProofNode.tsx
@@ -1,8 +1,10 @@
 import {
   Box, Button, Popover, PopoverArrow, PopoverCloseButton, PopoverContent,
-  PopoverHeader, PopoverTrigger, useDisclosure} from '@chakra-ui/react';
-import { ReactNode, useCallback, useState } from 'react';
-import { Handle, NodeProps, Position } from 'reactflow';
+  PopoverHeader, PopoverTrigger, useDisclosure
+} from '@chakra-ui/react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
+import Moveable from 'react-moveable';
+import { NodeProps } from 'reactflow';
 import { StatementNodeData } from '../../types/Node';
 import { localIndexToAbsolute } from '../../util/nodes';
 import SolveNodeModal from '../SolveNodeModal';
@@ -15,21 +17,28 @@ function ProofNode({ data }: NodeProps<StatementNodeData>) {
     data.thisNode.checkSyntax();
     data.thisNode.setWrappers();
   }, [data]);
+  const [target, setTarget] = useState<any>();
+  const [frame] = useState<any>({
+    translate: [0, 0],
+  });
+  useEffect(() => {
+    return setTarget(document.querySelector(".proof-node")!);
+  }, []);
 
   const [isCollapsed, setCollapsed] = useState(false);
   const { isOpen: isSolveNotReadyOpen, onOpen: onSolveNotReadyOpen, onClose: onSolveNotReadyClose } = useDisclosure();
   const { isOpen: isSolveModalOpen, onOpen: onSolveModalOpen, onClose: onSolveModalClose } = useDisclosure();
 
-  const checkSatButton: ReactNode = 
-    <Button size='xs' 
-      colorScheme='blackAlpha' 
-      onClick={() => { 
+  const checkSatButton: ReactNode =
+    <Button size='xs'
+      colorScheme='blackAlpha'
+      onClick={() => {
         data.thisNode.checkSyntax();
         onSolveModalOpen();
       }}>
       Solve
     </Button>;
-  
+
   const checkSolveReady = data.parsed === true;
   const solveNotReadyPopover =
     <Popover isOpen={isSolveNotReadyOpen} onClose={onSolveNotReadyClose}>
@@ -44,73 +53,98 @@ function ProofNode({ data }: NodeProps<StatementNodeData>) {
     </Popover>
 
   return (
-    <Box className="proof-node">
-      {/* BEGIN : Top Handle */}
-      <NodeHandle type='target'/>
-      {/* END : Top Handle */}
+    <div>
+      <Box className="proof-node">
+        {/* BEGIN : Top Handle */}
+        <NodeHandle type='target' />
+        {/* END : Top Handle */}
 
-      {isSolveModalOpen && <SolveNodeModal 
-        isOpen={isSolveModalOpen} 
-        onClose={onSolveModalClose} 
-        node={data}/>}
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-      {data.correctImplication === undefined &&
-      <Button colorScheme='whatsapp' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-        Check incoming implications
-      </Button>}
-      {data.correctImplication === "valid" &&
-        <Button colorScheme='whatsapp' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-          Check passed. Check again?
-        </Button>}
-      {data.correctImplication === "invalid" &&
-        <Button colorScheme='red' size='xs' onClick={() => {data.thisNode.checkEdges()}}>
-          Check failed. Check again?
-        </Button>}
-      </div>
-      {/* BEGIN: Givens */}
-      <StatementList 
-        title="Givens"
-        statements={data.givens}
-        callbacks={data.thisNode.givens}
-        indexToDisplayedIndex={index => localIndexToAbsolute(data, "givens", index)}
-        afterStatementEdit={afterStatementEdit}
+        {isSolveModalOpen && <SolveNodeModal
+          isOpen={isSolveModalOpen}
+          onClose={onSolveModalClose}
+          node={data} />}
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {data.correctImplication === undefined &&
+            <Button colorScheme='whatsapp' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check incoming implications
+            </Button>}
+          {data.correctImplication === "valid" &&
+            <Button colorScheme='whatsapp' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check passed. Check again?
+            </Button>}
+          {data.correctImplication === "invalid" &&
+            <Button colorScheme='red' size='xs' onClick={() => { data.thisNode.checkEdges() }}>
+              Check failed. Check again?
+            </Button>}
+        </div>
+        {/* BEGIN: Givens */}
+        <StatementList
+          title="Givens"
+          statements={data.givens}
+          callbacks={data.thisNode.givens}
+          indexToDisplayedIndex={index => localIndexToAbsolute(data, "givens", index)}
+          afterStatementEdit={afterStatementEdit}
+        />
+        {/* END: Givens */}
+
+
+        {/* BEGIN: Proof */}
+        <StatementList
+          title="Proof Steps"
+          statements={data.proofSteps}
+          callbacks={data.thisNode.proofSteps}
+          isCollapsed={isCollapsed}
+          indexToDisplayedIndex={index => localIndexToAbsolute(data, "proofSteps", index)}
+          afterStatementEdit={afterStatementEdit}
+        />
+        {/* END: Proof */}
+
+        {/* BEGIN: Goals */}
+        <StatementList
+          title="Goals"
+          statements={data.goals}
+          callbacks={data.thisNode.goals}
+          indexToDisplayedIndex={index => localIndexToAbsolute(data, "goals", index)}
+          afterStatementEdit={afterStatementEdit}
+        />
+        {/* END: Goals */}
+
+        {/* BEGIN: Node End Buttons */}
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
+          <DeleteNodePopover deleteNode={data.thisNode.delete} />
+          {data.proofSteps.length >= 3 && !isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => setCollapsed(true)}>Hide</Button>}
+          {isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => { setCollapsed(false) }}>Show</Button>}
+          {/* {checkSyntaxButton} */}
+          {checkSolveReady ? checkSatButton : solveNotReadyPopover}
+        </div>
+        {/* END: Node End Buttons */}
+
+        <NodeHandle type='source' />
+      </Box>
+      {/* BEGIN: Moveable component to allow horizontal resizing */}
+      <Moveable
+        target={target}
+        resizable={true}
+        keepRatio={false}
+        throttleResize={1}
+        renderDirections={["e", "w"]}
+        edge={false}
+        zoom={1}
+        origin={false}
+        padding={{ "left": 0, "top": 0, "right": 0, "bottom": 0 }}
+        onResizeStart={e => {
+          e.setOrigin(["%", "%"]);
+          e.dragStart && e.dragStart.set(frame.translate);
+        }}
+        onResize={e => {
+          const beforeTranslate = e.drag.beforeTranslate;
+          frame.translate = beforeTranslate;
+          e.target.style.width = `${e.width}px`;
+          e.target.style.transform = `translate(${beforeTranslate[0]}px, 0px)`;
+        }}
       />
-      {/* END: Givens */}
-
-
-      {/* BEGIN: Proof */}
-      <StatementList 
-        title="Proof Steps"
-        statements={data.proofSteps}
-        callbacks={data.thisNode.proofSteps}
-        isCollapsed={isCollapsed}
-        indexToDisplayedIndex={index => localIndexToAbsolute(data, "proofSteps", index)}
-        afterStatementEdit={afterStatementEdit}
-      />
-      {/* END: Proof */}
-
-      {/* BEGIN: Goals */}
-      <StatementList 
-        title="Goals"
-        statements={data.goals}
-        callbacks={data.thisNode.goals}
-        indexToDisplayedIndex={index => localIndexToAbsolute(data, "goals", index)}
-        afterStatementEdit={afterStatementEdit}
-      />
-      {/* END: Goals */}
-
-      {/* BEGIN: Node End Buttons */}
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
-        <DeleteNodePopover deleteNode={data.thisNode.delete} />
-        {data.proofSteps.length >= 3 && !isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => setCollapsed(true)}>Hide</Button>}
-        {isCollapsed && <Button size='xs' colorScheme='blackAlpha' onClick={() => { setCollapsed(false) }}>Show</Button>}
-        {/* {checkSyntaxButton} */}
-        {checkSolveReady ? checkSatButton : solveNotReadyPopover}
-      </div>
-      {/* END: Node End Buttons */}
-      
-      <NodeHandle type='source'/>
-    </Box>
+      {/* END: Moveable component to allow horizontal resizing */}
+    </div>
   );
 }
 
