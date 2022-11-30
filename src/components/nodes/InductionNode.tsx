36c937e01751600e63945229e71a8ed768289e93
Vlad Coroian <vladcoroian2001@gmail.com>
2022-11-30 20:19:51 +0000

    Add check edges for induction nodes to prove base/inductive cases

--- src/components/nodes/InductionNode.tsx
+++ src/components/nodes/InductionNode.tsx
@@ -10,31 +10,31 @@ import Statement from "../Statement";
 import { StatementType } from "../../types/Statement";
 import { DeleteNodePopover } from "./GeneralNode";
 
-function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>) : ReactElement {
+function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>): ReactElement {
 	const componentStyle = "induction-node";
-  const onChange = useCallback((evt: any, k: ListField<InductionNodeData>, updated: number) => {
-    nodeData.thisNode[k].update(updated, evt.target.value);
-  }, [nodeData]);
+	const onChange = useCallback((evt: any, k: ListField<InductionNodeData>, updated: number) => {
+		nodeData.thisNode[k].update(updated, evt.target.value);
+	}, [nodeData]);
 
-  const afterStatementEdit = useCallback(() => {
-    nodeData.thisNode.checkSyntax();
-  }, [nodeData]);
+	const afterStatementEdit = useCallback(() => {
+		nodeData.thisNode.checkSyntax();
+	}, [nodeData]);
 
-  const targetHandle: ReactNode = <Handle type="target" position={Position.Top} style={{ height: '10px', width: '10px' }} />;
-  const sourceHandle: ReactNode = <Handle type="source" position={Position.Bottom} id="b" style={{ height: '10px', width: '10px' }} />;
-  const checkSatButton: ReactNode = 
-    <Button size='xs'
-      colorScheme='blackAlpha' 
-      onClick={() => { 
-		nodeData.thisNode.checkPrinciple();
-      }}>
-      Solve
-    </Button>;
-	
+	const targetHandle: ReactNode = <Handle type="target" position={Position.Top} style={{ height: '10px', width: '10px' }} />;
+	const sourceHandle: ReactNode = <Handle type="source" position={Position.Bottom} id="b" style={{ height: '10px', width: '10px' }} />;
+	const checkSatButton: ReactNode =
+		<Button size='xs'
+			colorScheme='blackAlpha'
+			onClick={() => {
+				nodeData.thisNode.checkPrinciple();
+			}}>
+			Solve
+		</Button>;
+
 	const NodeBottomButtons = () => {
 		return (
 			<div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '5px' }}>
-        <DeleteNodePopover deleteNode={nodeData.thisNode.delete} />
+				<DeleteNodePopover deleteNode={nodeData.thisNode.delete} />
 				{checkSatButton}
 			</div>
 		);
@@ -51,7 +51,22 @@ function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>) : R
  */
 	return (
 		<Box className={componentStyle} key={`induction-node-${id}`}>
-      {targetHandle}
+			{targetHandle}
+
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				{nodeData.correctImplication === undefined &&
+					<Button colorScheme='whatsapp' size='xs' onClick={() => { nodeData.thisNode.checkEdges() }}>
+						Check incoming implications
+					</Button>}
+				{nodeData.correctImplication === "valid" &&
+					<Button colorScheme='whatsapp' size='xs' onClick={() => { nodeData.thisNode.checkEdges() }}>
+						Check passed. Check again?
+					</Button>}
+				{nodeData.correctImplication === "invalid" &&
+					<Button colorScheme='red' size='xs' onClick={() => { nodeData.thisNode.checkEdges() }}>
+						Check failed. Check again?
+					</Button>}
+			</div>
 			{/* BEGIN : Type */}
 			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
 				<Text>Type</Text>
@@ -64,23 +79,23 @@ function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>) : R
 			{/* END : Type */}
 
 			{/* BEGIN : Motive */}
-			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px'}}>
+			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px' }}>
 				<Text>Induction Goal</Text>
 			</div>
 			<Statement
 				onChange={e => onChange(e, "motive", 0)}
 				statement={nodeData.motive[0]}
 				index={0}
-				addAbove={() => {}}
-				addBelow={() => {}}
-				deleteStatement={() => {}}
+				addAbove={() => { }}
+				addBelow={() => { }}
+				deleteStatement={() => { }}
 				afterEdit={() => afterStatementEdit()}
-				proofNode={false}/>
+				proofNode={false} />
 			<Handle type="source" position={Position.Left} id="l" style={{ height: '10px', width: '10px' }} />
 			{/* END : Motive */}
 
 			{/* BEGIN : Base Case */}
-			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px'}}>
+			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px' }}>
 				<Text>Base Case(s)</Text>
 				<IconButton
 					variant='outline'
@@ -90,22 +105,23 @@ function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>) : R
 					onClick={() => { nodeData.thisNode.baseCases.add() }}
 				/>
 			</div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        {nodeData.baseCases.map((s: StatementType, index: number) =>
-          <Statement
-            onChange={e => onChange(e, "baseCases", index)}
-            statement={s}
-            index={index}
-            proofNode={true}
-            addAbove={() => { nodeData.thisNode.baseCases.add(index) }}
-            addBelow={() => { nodeData.thisNode.baseCases.add(index + 1) }} 
-            deleteStatement = {() => {nodeData.thisNode.baseCases.remove(index)}}
-						key={index}/>)}
-      </div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				{nodeData.baseCases.map((s: StatementType, index: number) =>
+					<Statement
+						onChange={e => onChange(e, "baseCases", index)}
+						statement={s}
+						index={index}
+						proofNode={true}
+						addAbove={() => { nodeData.thisNode.baseCases.add(index) }}
+						addBelow={() => { nodeData.thisNode.baseCases.add(index + 1) }}
+						deleteStatement={() => { nodeData.thisNode.baseCases.remove(index) }}
+						afterEdit={() => afterStatementEdit()}
+						key={index} />)}
+			</div>
 			{/* END : Base Case */}
 
 			{/* BEGIN : Induction Case */}
-			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px'}}>
+			<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5px' }}>
 				<Text>Inductive Case(s)</Text>
 				<IconButton
 					variant='outline'
@@ -115,24 +131,25 @@ function InductionNode({ id, data: nodeData }: NodeProps<InductionNodeData>) : R
 					onClick={() => { nodeData.thisNode.inductiveCases.add() }}
 				/>
 			</div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        {nodeData.inductiveCases.map((s: StatementType, index: number) =>
-          <Statement
-            onChange={e => onChange(e, "inductiveCases", index)}
-            statement={s}
-            index={index}
-            proofNode={true}
-            addAbove={() => { nodeData.thisNode.inductiveCases.add(index) }}
-            addBelow={() => { nodeData.thisNode.inductiveCases.add(index + 1) }} 
-            deleteStatement = {() => {nodeData.thisNode.inductiveCases.remove(index)}}
-						key={index}/>)}
-      </div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				{nodeData.inductiveCases.map((s: StatementType, index: number) =>
+					<Statement
+						onChange={e => onChange(e, "inductiveCases", index)}
+						statement={s}
+						index={index}
+						proofNode={true}
+						addAbove={() => { nodeData.thisNode.inductiveCases.add(index) }}
+						addBelow={() => { nodeData.thisNode.inductiveCases.add(index + 1) }}
+						deleteStatement={() => { nodeData.thisNode.inductiveCases.remove(index) }}
+						afterEdit={() => afterStatementEdit()}
+						key={index} />)}
+			</div>
 			{/* END : Induction Case */}
 
 			{/* START : Node Bottom Buttons */}
 			<NodeBottomButtons />
 			{/* END : Node Bottom Buttons */}
-      {sourceHandle}
+			{sourceHandle}
 		</Box>
 
 	)
